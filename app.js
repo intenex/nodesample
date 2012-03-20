8792ca21c68150f2e9d735449ca76238d6b8390b
anoopsinha <anoop.k.sinha@gmail.com>
2012-03-19 22:16:02 -0700

    adding jqtpl template and search results

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 var http = require('http');
 var express = require('express');
+var jqtpl = require("jqtpl");
+
 var app = express.createServer();
 
+
 app.configure(function(){
+    app.set("view engine", "html");
+    app.register(".jqtpl", jqtpl.express);
+    app.set("view options", { layout: false })
+    app.set('views', __dirname + '/views');
     app.use(express.methodOverride());
     app.use(express.bodyParser());
     app.use(app.router);
     app.use(express.static(__dirname + '/public'));
 });
 
-app.get('/', function(req, res){
-        res.sendfile('index.html', {layout:false});
 
+app.get('/', function(req, res){
+       res.render('index.html.jqtpl', {
+	       results: ""
+	   });
     });
+
 
 app.post('/', function(req, res){
     
@@ -21,7 +31,6 @@ app.post('/', function(req, res){
 
     var query = req.param('query', null);
     var url_string = 'http://localhost:9200/_search?q='+escape(query)+'&pretty=true';
-
     var t_client = http.createClient(9200, "localhost");  
     var request = t_client.request("GET", "/_search?q="+escape(query), {"host": "localhost"});  
   
@@ -30,11 +39,11 @@ app.post('/', function(req, res){
         response.addListener("data", function(data) {  
             body += data;  
         });  
-  
         response.addListener("end", function() { 
             console.log(body);
-
-	    res.send(body);
+	    res.render('index.html.jqtpl', {
+			results: body
+		});
         });  
     });  
 
